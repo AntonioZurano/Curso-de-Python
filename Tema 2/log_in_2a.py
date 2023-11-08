930fee7b2093ba0175e70fa4730c0b2c664013eb
azuranob <azurano@outlook.es>
2023-11-08 01:08:01 +0100

    Add password verification and error handling to log in process

--- Tema 2/log_in_2a.py
+++ Tema 2/log_in_2a.py
@@ -6,9 +6,20 @@ password= input("Introduce la contraseña: ")
 
 # --- Comprobamos si la contraseña es correcta --- #
 if password == key:
+    # --- Si coincide damos un la bienvenida --- #
     print("Contraseña correcta. ¡Bienvenido!")
-    
-# --- Si coincide damos un la bienvenida --- #
-# Si no coincide mostramos un error y pedimos la contraseña de nuevo--- #
-## si esta vez la contraseña coincide damos la bienvenida
-## si no damos un mensaje de error y termina el programa
+else:
+    # Si no coincide mostramos un error y pedimos la contraseña de nuevo--- #
+    print("Error, la contraseña no es correcta.")   
+    # --- Pedimos la contraseña al usuario --- #
+    password= input("Introduce la contraseña: ")
+    if password == key:
+        ## si esta vez la contraseña coincide damos la bienvenida
+        print("Contraseña correcta. ¡Bienvenido!")
+    else:
+        ## si no damos un mensaje de error y termina el programa
+
+
+
+
+
