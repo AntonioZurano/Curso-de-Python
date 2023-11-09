63990a6174d86a9c04bbd972a74a65b42f266892
azuranob <azurano@outlook.es>
2023-11-10 00:55:07 +0100

    Refactor mayor_cuatro.py to simplify code and improve readability

--- Tema 2/mayor_cuatro.py
+++ Tema 2/mayor_cuatro.py
@@ -1,22 +1,23 @@
 # --- Pedir NUMEROS al usuario y decir cual es el mayor de los cuatro --- #
 # --- Pedimos los numeros al usuario --- #
-numero1 = input("Introduce el primer número: ")
-numero1 = int(numero1)
-numero2 = input("Introduce el segundo número: ")
-numero2 = int(numero2)
-numero3 = input("Introduce el tercer número: ")
-numero3 = int(numero3)
-numero4 = input("Introduce el cuarto número: ")
-numero4 = int(numero4)
+a = input("Introduce el primer número: ")
+a = float(a)
+b = input("Introduce el segundo número: ")
+b = float(b)
+c = input("Introduce el tercer número: ")
+c = float(c)
+d = input("Introduce el cuarto número: ")
+d = float(d)
 
 # --- Comprobamos cual es el mayor --- #
-if numero1 >= numero2 and numero1 >= numero3 and numero1 >= numero4:
-    print("El número mayor es: ", numero1)
-elif numero2 >= numero1 and numero2 >= numero3 and numero2 >= numero4:
-    print("El número mayor es: ", numero2)
-elif numero3 >= numero1 and numero3 >= numero2 and numero3 >= numero4:
-    print("El número mayor es: ", numero3)
-else:
-    print("El número mayor es: ", numero4)
+if (a>b):
+    b = a
+
+if (b>c):    
+    c = b
+
+if (c>d):
+    d = c
+print("El mayor de los cuatro es: ", d)
 
 
