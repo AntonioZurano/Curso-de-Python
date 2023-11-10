233d41b30cd4b6c72d58951d8c5d8c630024685f
azuranob <azurano@outlook.es>
2023-11-10 01:07:39 +0100

    version aun mas eficiente

--- Tema 2/relacion_numeros_extra.py
+++ Tema 2/relacion_numeros_extra.py
@@ -7,3 +7,12 @@ if x == y+z or y == x+z or z == x+y:
     print("True")   
 else:
     print("False")
+
+
+# otra forma con una sola linea
+# Pedir al usuario 3 numeros diferentes
+x = int(input("Introduce el primer número: "))      
+y = int(input("Introduce el segundo número: "))
+z = int(input("Introduce el tercer número: "))
+
+print(x == y+z or y == x+z or z == x+y)
