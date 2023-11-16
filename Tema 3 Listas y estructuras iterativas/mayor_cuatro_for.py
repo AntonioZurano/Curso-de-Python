8eca550ddd8e1d4171e3c3e407b82b01bae79a60
azuranob <azurano@outlook.es>
2023-11-16 01:45:48 +0100

    Ejercicio de crear y recorrer lista con for

---
+++ Tema 3 Listas y estructuras iterativas/mayor_cuatro_for.py
@@ -0,0 +1,24 @@
+'''
+Pide a usuario 4 números y devuelve 
+los números introducidos en orden 
+descendente. Para ello puedes usar listas y bucles for.'''
+
+# --- inicializar una lista de numeros
+lista_numeros = []
+
+# --- Crear un bucle para ir pidiendo los números
+# --- y añadiendolos a la lista
+
+for i in range(0,4):
+    # pido el numero
+    numero = input("Dime un numero: ")
+    # lo convierto a entero
+    numero = int(numero)
+    # lo añado a la lista de numeros
+    lista_numeros.append(numero)
+# output: lista con cuatro numeros
+print(lista_numeros)
+# --- imprimo el mayor numero de la lista por pantalla
+print("El número mas alto es",max(lista_numeros))
+# --- imprimir la lista ordenada 
+print("La lista ordenada es",sorted(lista_numeros))
