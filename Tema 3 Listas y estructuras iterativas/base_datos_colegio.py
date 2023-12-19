fe84275110935b09a78ecc6028c6e542416804c6
azuranob <azurano@outlook.es>
2023-12-19 11:26:39 +0100

    enunciado base datos colegio

---
+++ Tema 3 Listas y estructuras iterativas/base_datos_colegio.py
@@ -0,0 +1,10 @@
+'''
+Trabajas en un colegio y estas encargado de mantener un seguimiento de las notas de los estudiantes de una clase.
+En tu base de datos tienes una lista con los nombres de los estudiantes, y para cada estudiante debes guardar sus notas
+provenientes de deberes, exámenes y proyectos.
+También necesitas calcular la nota media de cada estudiante y la nota media de la clase al completo.
+
+
+Pista: Para resolver este problema, puedes usar una lista anidada donde guardes las notas para cada estudiante. Entonces
+puedes usar un bucle para recorrer la lista y 
+'''
