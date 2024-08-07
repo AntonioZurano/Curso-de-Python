a3ffdcb0790a63e9ede72aab77b4fefb1421237c
Antonio Zurano <azurano@outlook.es>
2024-08-07 12:22:55 +0200

    feat: Add user validation to welcome message bienvenida_2a

--- Tema 2 Test Condicionales, if statements y switch-case/bienvenida_2a.py
+++ Tema 2 Test Condicionales, if statements y switch-case/bienvenida_2a.py
@@ -7,24 +7,29 @@ usuario invitado'''
 
 # --- Pedimos el nombre de usuario
 nombre=input("Introduce tu nombre de usuario: ")
+# --- Variables con nombres de los usuarios
+# --- que pueden acceder al sistema
+usuario_1="alejandro"
+usuario_2="naomi"
+usuario_3="sergio"
 # --- Convertimos el nombre de usuario a minúsculas
 # --- para que no haya problemas con las mayúsculas
 # --- y minúsculas
 nombre=nombre.lower()
 # --- eliminamos los espacios en blanco que pueda tener
-nombre=nombre.replace(" ","" )
+nombre=nombre.replace(" ","")
 # --- eliminamos las comillas que pueda tener
 nombre=nombre.replace("'","")
 # --- eliminamos los puntos que pueda tener
 nombre=nombre.replace(".","")
 # --- Comprobamos si es uno de los tres usuarios
-if nombre=="alejandro" or nombre=="naomi" or nombre=="sergio":
+if nombre==usuario_1 or nombre==usuario_2 or nombre==usuario_3:
     # --- Si es uno de los tres usuarios damos la bienvenida personalizada
-    print("Bienvenido "+nombre)
+    print("Bienvenido "+nombre.title())
 
 # --- Si es el usuario invitado damos la bienvenida general
 elif nombre=="invitado":
-    print("Bienvenido invitado")
+    print("Bienvenido usuario invitado")
 # --- Si no es ninguno de los anteriores damos un mensaje de error
 else:
     print("Usuario no reconocido")
