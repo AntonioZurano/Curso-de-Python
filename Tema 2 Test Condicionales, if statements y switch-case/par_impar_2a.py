ad715e779aebfe003095e557c41c3759b059f192
Antonio Zurano <azurano@outlook.es>
2024-08-07 12:41:34 +0200

    chore: Corregir mensaje de número par/impar en par_impar_2a.py

--- Tema 2 Test Condicionales, if statements y switch-case/par_impar_2a.py
+++ Tema 2 Test Condicionales, if statements y switch-case/par_impar_2a.py
@@ -2,7 +2,10 @@
 numero = input("Introduce un número: ")
 numero = int(numero)
 
+# --- Comprobar si el número es par o impar
+# si es un multiplo de 2 será par
 if numero % 2 == 0:
-    print("El número es par.")
+    print("El número", numero, "es par.")
+# si no será impar
 else:
-    print("El número es impar.")
+    print("El número", numero, "es impar.")
