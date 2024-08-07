aad7ded5cdf27811aa8de8ef10daee584a256817
Antonio Zurano <azurano@outlook.es>
2024-08-07 12:57:47 +0200

    chore: Corregir indentación y espaciado en el código de mayor_cuatro_2b.py

--- Tema 2 Test Condicionales, if statements y switch-case/mayor_cuatro_2b.py
+++ Tema 2 Test Condicionales, if statements y switch-case/mayor_cuatro_2b.py
@@ -1,13 +1,9 @@
 # --- Pedir NUMEROS al usuario y decir cual es el mayor de los cuatro --- #
 # --- Pedimos los numeros al usuario --- #
-a = input("Introduce el primer número: ")
-a = float(a)
-b = input("Introduce el segundo número: ")
-b = float(b)
-c = input("Introduce el tercer número: ")
-c = float(c)
-d = input("Introduce el cuarto número: ")
-d = float(d)
+a = float(input("Introduce el primer número: "))
+b = float(input("Introduce el segundo número: "))
+c = float(input("Introduce el tercer número: "))
+d = float(input("Introduce el cuarto número: "))
 
 # --- Comprobamos cual es el mayor --- #
 if (a>b):
@@ -18,7 +14,7 @@ if (b>c):
 
 if (c>d):
   c, d = d , c
-  
+
 print("El mayor de los cuatro es: ", d)
 print("El orden de los números de menor a mayor es: ", a, b, c, d)
 
